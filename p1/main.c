063edafe49f6cc2c9a786e742e2f44f95ab84a0b
2025050066 <hrhr0107@gmail.com>
2026-04-29 16:08:02 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -3,7 +3,7 @@ int main() {
 	int amount;
 	scanf("%d", &amount);
 	int A,B,C;
-	int jouge = false;
+	bool jouge = false;
 	for (A = 1; A <= amount / 900; A++) {
 		for (B = 2; B <= amount / 750; B += 2) {
 			for (C = 1; C <= amount / 200; C++) {
