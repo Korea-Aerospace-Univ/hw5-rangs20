be928598c9f20e0dea45e30bd0d35e34a9f47a70
2025050066 <hrhr0107@gmail.com>
2026-04-29 15:53:30 +0900

    Update main.c

---
+++ p1/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>	
+int main() {
+	int amount;
+	scanf("%d", &amount);
+	int A,B,C;
+	int jouge = 0;
+	for (A = 1; A <= amount / 900; A++) {
+		for (B = 2; B <= amount / 750; B += 2) {
+			for (C = 1; C <= amount / 200; C++) {
+				if ((900 * A) + (B * 750) + (C * 200) == amount) {
+					if (C < A || C < B)
+					{
+						printf("%d %d %d\n", A, B, C);
+						jouge = 1;
+					}
+				}		
+			}
+		}
+	}
+	if (jouge == 0) {
+		printf("none");
+	}
+}
